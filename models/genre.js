9a4f1eb85d215b742e7be66653cecbb3b109c464
zee24 <46341377@qq.com>
2024-10-14 08:35:52 +0800

    增加了genre的访问数据库相关路由

---
+++ models/genre.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
+
+const Genre = mongoose.model("Genre", genreSchema);
+
+exports.Genre = Genre;
--- routes/genres.js
+++ routes/genres.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const { Genre } = require("../models/genre");
 
 const router = express.Router();
 
-router.get("/api/genres", (req, res) => {
-  res.send("This is genre page");
+router.get("/api/genres", async (req, res) => {
+  try {
+    const genres = await Genre.find();
+    res.send(genres);
+  } catch (ex) {
+    // Log sth on the server
+    // 500 and sent
+    res.status(500).send("Sth wrong happened");
+  }
 });
 
 module.exports = router;
